0429f399350fb08ca5a66d09e93d8cdc8f7cfeb3
Nikos <nikosglikis@gmail.com>
2016-04-20 02:15:51 -0700

    Better rl read.

--- src/main/java/com/object0r/toortools/Utilities.java
+++ src/main/java/com/object0r/toortools/Utilities.java
@@ -230,7 +230,7 @@ public class Utilities
             conn.setRequestProperty("Cookie",cookie);
             BufferedReader in = new BufferedReader( new InputStreamReader(conn.getInputStream(), encoding ) );
 
-            String inputLine;
+            /*String inputLine;
             StringBuffer sb = new StringBuffer();
             while ((inputLine = in.readLine()) != null)
             {
@@ -238,6 +238,15 @@ public class Utilities
 
             }
             in.close();
+            return sb.toString();*/
+
+            StringBuffer sb = new StringBuffer();
+            int BUFFER_SIZE=1024;
+            char[] buffer = new char[BUFFER_SIZE]; // or some other size,
+            int charsRead = 0;
+            while ( (charsRead  = in.read(buffer, 0, BUFFER_SIZE)) != -1) {
+                sb.append(buffer, 0, charsRead);
+            }
             return sb.toString();
         }
         catch (FileNotFoundException e)
