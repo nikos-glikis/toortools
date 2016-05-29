7658429f43a273d6433098ad73a635cdf12847fb
nikos <nikosglikis@gmail.com>
2016-05-29 23:16:02 +0300

    SendEmailHelper.java

---
+++ src/main/java/com/object0r/toortools/helpers/SendEmailHelper.java
@@ -0,0 +1,25 @@
+package com.object0r.toortools.helpers;
+
+
+import com.object0r.toortools.os.OsHelper;
+
+public class SendEmailHelper
+{
+    /**
+     * Sends an email if you are on a Linux Server.
+     * @param to
+     * @param subject
+     * @param body
+     */
+    public void sendEmail(String to, String subject, String body)
+    {
+        try
+        {
+            OsHelper.runCommand("echo \""+body.replace("\"","\\\"")+"\" | tee | mail -s \""+subject.replace("\"","\\\"")+"\" \""+to+"\"");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
