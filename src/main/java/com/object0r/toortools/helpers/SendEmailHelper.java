436cc813c762967f10b1a8f04415c269dd938bfc
nikos <nikosglikis@gmail.com>
2016-10-31 19:03:27 +0200

    Improoved mail.

--- src/main/java/com/object0r/toortools/helpers/SendEmailHelper.java
+++ src/main/java/com/object0r/toortools/helpers/SendEmailHelper.java
@@ -1,13 +1,20 @@
 package com.object0r.toortools.helpers;
 
 
+import com.object0r.toortools.Utilities;
 import com.object0r.toortools.os.OsCommandOutput;
 import com.object0r.toortools.os.OsHelper;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.log4j.or.ThreadGroupRenderer;
+import org.fusesource.leveldbjni.internal.Util;
+
+import java.io.File;
 
 public class SendEmailHelper
 {
     /**
      * Sends an email if you are on a Linux Server.
+     *
      * @param to
      * @param subject
      * @param body
@@ -16,12 +23,22 @@ public class SendEmailHelper
     {
         try
         {
+            String tmpDir = "/tmp/toortools_mail/";
+            if (!new File(tmpDir).exists())
+            {
+                new File(tmpDir).mkdirs();
+            }
+            String tmpFile = tmpDir + RandomStringUtils.randomAlphanumeric(8);
+            Utilities.writeStringToFile(tmpFile, body);
             String[] cmd = {
                     "/bin/sh",
                     "-c",
-                    "echo \""+body.replace("\"","\\\"")+"\" | tee | mail -s \""+subject.replace("\"","\\\"").replace("\n"," ")+"\" \""+to+"\""
+                    //"echo \""+body.replace("\"","\\\"")+"\" | tee | mail -s \""+subject.replace("\"","\\\"").replace("\n"," ")+"\" \""+to+"\""
+                    "cat " + tmpFile + " | tee | mail -s \"" + subject.replace("\"", "\\\"").replace("\n", " ") + "\" \"" + to + "\""
             };
             Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+            new File(tmpFile).delete();
             //System.out.println(cmd[3]);
             //p.waitFor();
         }
