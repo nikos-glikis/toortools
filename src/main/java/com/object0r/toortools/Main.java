76ce363bf098182529a0a9f347f8793be9c1204e
Nikos <nikosglikis@gmail.com>
2018-06-16 10:10:36 +0300

    - Now Utilities.Trusteverybody() does not use deprecated code.

--- src/main/java/com/object0r/toortools/Main.java
+++ src/main/java/com/object0r/toortools/Main.java
@@ -1,5 +1,8 @@
 package com.object0r.toortools;
 
+import com.object0r.toortools.http.HttpHelper;
+import com.object0r.toortools.http.HttpRequestInformation;
+import com.object0r.toortools.http.HttpResult;
 import com.object0r.toortools.os.OsHelper;
 import com.object0r.toortools.os.RecurringProcessHelper;
 
@@ -9,7 +12,21 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println(OsHelper.getPidInformationLine(9308));
+        Utilities.trustEverybody();
+        HttpRequestInformation httpRequestInformation = new HttpRequestInformation();
+        httpRequestInformation.setUrl("https://self-signed.badssl.com/");
+        httpRequestInformation.setThrowExceptions(true);
+        HttpResult httpResult = null;
+        try
+        {
+            httpResult = HttpHelper.request(httpRequestInformation);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println(httpResult.getContentAsString());
+        System.out.println(httpResult.getResponseCode());
         System.exit(0);
         /*
         test proxies
