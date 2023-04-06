5fc432b808b6a650589183103e251b7bbe293cf7
Nikos Glikis <nikosglikis@gmail.com>
2023-04-06 18:12:24 +0300

    - Reduce default ssh command timeout - Timeout now is a parameter.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+#export JAVA_HOME=/home/nikos/.jdks/corretto-11.0.13/
 #mvn install:install-file  -DgroupId=com.object0r.toortools -DartifactId=toortools -Dversion=1.0.2 -Dpackaging=jar -Dfile=target/toortools-1.0.2-jar-with-dependencies.jar
 #mvn install:install-file -DgroupId=ftp4j -DartifactId=ftp4j -Dversion=1.7.2 -Dpackaging=jar -Dfile=lib/ftp4j/ftp4j/1.7.2/ftp4j-1.7.2.jar
 
